047b75770c0d3b5ae7f6ce63649bdf3d90501bcb
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-08-19 09:54:26 +0200

    kdjnjkfsn

--- api/controller/index.js
+++ api/controller/index.js
@@ -1,7 +1,27 @@
-const { users, products } = require("../model");
+const { users, products, cart } = require("../model");
 const express = require("express");
 const routes = express.Router();
+const bodyParser = require('body-parser')
 
+// ========== user routes ==========
+routes.get("/users", (req, res)=>{
+    users.fetchUsers(req, res)
+})
+routes.get("/user/:id", (req, res)=>{
+    users.fetchUser(req, res)
+})
+routes.post("/login", bodyParser.json(), (req, res)=>{
+    users.login(req, res)
+})
+routes.register("/register", bodyParser.json(), (req, res)=>{
+    users.register(req, res)
+})
+routes.patch("/user/:id", bodyParser.json(), (req, res)=>{
+    users.updateUser(req, res)
+})
+routes.delete("/user/:id", bodyParser.json(), (req, res)=>{
+    users.removeUser(req, res)
+})
 
 module.exports = {
   express,
--- api/model/Cart.js
+++ api/model/Cart.js
@@ -1,1 +1,54 @@
 const db = require('../config')
+
+class Cart{
+    fetchItems(req, res){
+        const query = `
+            SELECT cartID, userID, productID, QUANTITY FROM CART
+        `
+        db.query(query, (err, results)=>{
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                results
+            })
+        })
+    }
+    fetchItem(req, res){
+        const query = `
+            SELECT cartID, userID, productID, QUANTITY FROM CART WHERE cartID = ${req.params.cartID}
+        `
+        db.query(query, [req.params.cartID], (err, result)=>{
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                result
+            })
+        })
+    }
+    insertItem(req, res){
+        const query = `
+            INSERT INTO Cart VALUES(?, ?, ?, ?)
+        `
+        db.query(query, [req.body, req.params.userID, req.params.productID, req.params.QUANTITY], (err)=>{
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "Item added to cart"
+            })
+        })
+    }
+    deleteItem(req, res){
+        const query = `
+            DELETE FROM CART WHERE cartID = ${req.params.cartID}
+        `
+        db.query(query, [req.params.cartID], (err)=>{
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "Item removed from cart"
+            })
+        })
+    }
+}
+
+module.exports = Cart
